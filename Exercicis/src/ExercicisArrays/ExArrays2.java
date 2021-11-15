8ddf6612154a63f24a389c5d25f1deb715010b32
Your Name <you@example.com>
2021-11-15 14:14:37 +0100

    startar

--- Exercicis/src/ExercicisArrays/ExArrays2.java
+++ Exercicis/src/ExercicisArrays/ExArrays2.java
@@ -2,7 +2,7 @@ package ExercicisArrays;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class ExArrays1 {
+public class ExArrays2 {
     public static void main(String[] args) {
         int[] ar = {1,3,-2,15,2};
 
