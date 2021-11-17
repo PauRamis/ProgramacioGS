cd0d99bdf92632691a50ebdbe7d838c89ba3bc10
Your Name <you@example.com>
2021-11-17 18:10:10 +0100

    startteoria

---
+++ Exercicis/src/ExercicisArrays/ExArrays1.java
@@ -0,0 +1,24 @@
+package ExercicisArrays;
+
+public class ExArrays1 {
+    /*
+1 - Alguns programes empren «int a[]» enlloc de «int[] a», quina és la diferència?.
+
+«int a[]» és va crear per ajudar als programadors de C a acostumbrarse a java, però «int[] a» és més usada i facil d’interpretar pels programadors acostumats a java.
+No hi ha una diferencia real per la maquina.
+
+2 - Per què els índexs en els arrays comencen en 0 i no en 1?
+
+Perque els ordinadors començen a comptar desde el 0
+
+
+3 - Què passa si emprem un número negatiu per indexar un array?
+
+Si intentasis accedir a una posició negativa de l’array, simplement donarà error, ja que comença desde el 0, i no n’hi ha més darrere.
+
+4 -
+
+5 -
+
+     */
+}
