77097af3bdd5a39c138fc32d879d5d6a6145ca17
Your Name <you@example.com>
2022-01-14 16:58:00 +0100

    MesuraDia

--- Exercicis/src/ExercicisTemperatures/MesuraDia.java
+++ Exercicis/src/ExercicisTemperatures/MesuraDia.java
@@ -1,7 +1,16 @@
 package ExercicisTemperatures;
 
+import java.time.LocalDate;
+
 public class MesuraDia {
     int tMitjana;
+    boolean tMitjanaExists;
     int tMin;
+    boolean tMinExists;
     int tMax;
+    boolean tMaxExists;
+    double precip;
+    boolean precipExists;
+    LocalDate data;
+
 }
