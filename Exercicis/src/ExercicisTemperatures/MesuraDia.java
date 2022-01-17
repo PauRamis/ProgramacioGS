27824f400415a5b62b373d0067565b3ba9cea500
Your Name <you@example.com>
2022-01-17 14:07:30 +0100

    ComençMitjana

--- Exercicis/src/ExercicisTemperatures/MesuraDia.java
+++ Exercicis/src/ExercicisTemperatures/MesuraDia.java
@@ -13,4 +13,19 @@ public class MesuraDia {
     boolean precipExists;
     LocalDate data;
 
+    /*    //Constructores
+
+    MesuraDia(int tempMitjana, int  tempMin, int tempMax){
+        tMitjana = tempMitjana;
+        tMin = tempMin;
+        tMax = tempMax;
+    }
+    //Metodos
+    public static void main(String[] args) {
+        MesuraDia avui = new MesuraDia(12, 12, 12);
+
+        MesuraDia dema = new MesuraDia(13, 13, 13);
+    }
+}
+*/
 }
--- Exercicis/src/ExercicisTemperatures/Temperatures.java
+++ Exercicis/src/ExercicisTemperatures/Temperatures.java
@@ -11,13 +11,22 @@ public class Temperatures {
     //El main es dedica a mostrar el menu principal, i dirigir a l'usuari cap a les dades o a sortir.
     public static void main(String[] args) {
         while (true) {
+
+            //Primer mostram el menu principal perque l'usuari decidesqui que vol fer
             int opcio = MostrarMenuPrincipal();
             if (opcio == 1) {
 
                 //Dirigim a l'usuari cap a la funcio de les dades
                SeleccionarDades();
 
-            } else if (opcio == 2) {
+            } else if (opcio == 2){
+                FerMitjana();
+
+            } else if (opcio == 3){
+                System.out.println("Encara en producció..");
+
+
+            } else if (opcio == 4) {
 
                 //Feim un return per acabar el programa
                 return;
@@ -27,10 +36,15 @@ public class Temperatures {
         }
     }
 
+
+
+    //Aquesta funció exposa totes les opcions al usuari i les torna al main
     private static int MostrarMenuPrincipal() {
-        System.out.println("1 = Mostrar les dades de un més");
-        System.out.println("2 = Sortir");
-        System.out.println("3 = ");
+
+        System.out.println("1 = Mostrar la temperatura de un dia de un mes");
+        System.out.println("2 = Mitjana de totes les dades de un mes");
+        System.out.println("3 = Mitjana de totes les dades entre un inveral a triar");
+        System.out.println("4 = Sortir");
         int option = sc.nextInt();
         return option;
     }
@@ -41,12 +55,12 @@ public class Temperatures {
         if (1 <= nMes & nMes <= 12) {
             System.out.println("Dia del mes? (01 - 31)");
             int nDia = sc.nextInt();
-            calcDades(nMes, nDia);
+            calcDiaDelMes(nMes, nDia);
         } else System.out.println("NombreInvalid");
 
     }
 
-    private static void calcDades(int nMes, int nDia) {
+    private static void calcDiaDelMes(int nMes, int nDia) {
         //Primer farem un array amb totes les linies de contingut de Temps.
          String data = Temps.data;
          String[] linea = data.split("\n");
@@ -68,11 +82,14 @@ public class Temperatures {
 
             if (mes == Month.of(nMes)){
                 if (dia == nDia){
-                    System.out.print("Tavg: ");
+                    System.out.print("La temperatura va ser: ");
                     System.out.println(uDada[5]);
                 }
             }
         }
+    }
+
+    private static void FerMitjana() {
     }
 
 }
